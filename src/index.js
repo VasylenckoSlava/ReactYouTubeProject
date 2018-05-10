e25967380c195f0d38fee8b5838283e3f689d639
VasylenckoSlava <slavavasylencko@gmail.com>
2018-05-10 11:12:54 +0300

    Create components and add youtubesearchapi

--- src/index.js
+++ src/index.js
@@ -4,5 +4,7 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+const API_KEY = 'AIzaSyBLAlD6-wxTCssOU6SDuoEcTyVnAd_TAew';
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
